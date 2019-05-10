eec2ea9c15b2dea4e7f27887c26316345de257be
luis1pinheiro1 <lepinheiro@outlook.com>
2019-05-10 10:19:47 -0300

    alteracao

--- V1.py
+++ V1.py
@@ -29,32 +29,23 @@ YELLOW = (255, 255, 0)
 
 # Classe Jogador que representa a nave
 class Player(pygame.sprite.Sprite):
-    
     # Construtor da classe.
     def __init__(self):
-        
         # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
-        
         # Carregando a imagem de fundo.
         player_img = pygame.image.load(path.join(img_dir, "Dino.png")).convert()
         self.image = player_img
-        
         # Diminuindo o tamanho da imagem.
         self.image = pygame.transform.scale(player_img, (50, 38))
-        
         # Deixando transparente.
         self.image.set_colorkey(BLACK)
-        
         # Detalhes sobre o posicionamento.
         self.rect = self.image.get_rect()
-        
         # Centraliza embaixo da tela.
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
-        
         self.speedy = 0
-        
     def update(self):
         self.rect.y += self.speedy
         if self.rect.space > HEIGHT:
