e05521316083905a25f177446207a2148092a87a
Diogo Cintra <diogonac@al.insper.edu.br>
2019-05-14 16:35:10 -0300

    alteracao

--- V1.py
+++ V1.py
@@ -60,22 +60,19 @@ class Mob(pg.sprite.Sprite):
         self.rect.x=px
         #sorteia lugar y
         self.rect.y=py
-<<<<<<< HEAD
         #sorteia velocidade inicial
         self.speedx= 3
         self.speedy= 0
         
-=======
+
         self.image.set_colorkey(WHITE)
->>>>>>> 11978ef139b79b9d1f06310a0568a4c5227ad31f
          
-        Mob_rect1.x -=3              
-        background_rect2.x -=3
-        screen.blit(background, background_rect1)
-        screen.blit(background, background_rect2)
-        if background_rect1.x<0:
-            background_rect1.x=WIDTH
-            background_rect2.x = 0
+        #Mob_rect1.x -=3              
+        #background_rect2.x -=3
+        #screen.blit(background, background_rect1)
+        #screen.blit(background, background_rect2)
+        #if background_rect1.x<0:
+         ##   background_rect2.x = 0
       
         
         #se as plantas passarem da tela volta para o lado
