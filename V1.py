8d4a832fee667a9a2d1bc0ae827ee0f2f6a85784
LauraPerim <laurabatmanp@gmail.com>
2019-05-14 16:37:56 -0300

    alteracao

--- V1.py
+++ V1.py
@@ -60,9 +60,12 @@ class Mob(pg.sprite.Sprite):
         self.rect.x=px
         #sorteia lugar y
         self.rect.y=py
-        self.image.set_colorkey(WHITE)
+        #sorteia velocidade inicial
+        self.speedx=-3
+        self.speedy= -3
+        
          
-        Mob_rect1.x -=3              
+        background_rect1.x -=3              
         background_rect2.x -=3
         screen.blit(background, background_rect1)
         screen.blit(background, background_rect2)
