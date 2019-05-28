09b9871645d2137536bdf43aab93cb417ca432ce
LauraPerim <laurabatmanp@gmail.com>
2019-05-28 17:26:33 -0300

    alteracao luis

--- V1.py
+++ V1.py
@@ -238,7 +238,7 @@ def tela_play():
             all_sprites.add(mob)
             all_mobs.add(mob)
             cont_Mob = 0
-            intervalo_Mob = random.randint(1, 100)
+            intervalo_Mob = random.randint(50, 100)
 
         if cont_aguia == intervalo_aguia:
             aguia = Aguia(WIDTH, 235)
@@ -246,7 +246,7 @@ def tela_play():
             all_sprites.add(aguia)
             all_aguias.add(aguia)
             cont_aguia = 0
-            intervalo_aguia = random.randint(10, 1000)            
+            intervalo_aguia = random.randint(500, 1000)            
             
         
         for event in pg.event.get():
@@ -292,7 +292,6 @@ def tela_play():
             all_sprites.add(vida)
             all_vida.add(vida)
             cont_life = 0
-<<<<<<< HEAD
             prob_life = random.randint(240,1000)
             
              # Desenha as vidas
@@ -302,11 +301,8 @@ def tela_play():
 #        screen.blit(text_surface, text_rect)
         print(player.vida)
             
-  
-            
-=======
-            prob_vida = random.randint(240,1000)
->>>>>>> d590631ae2ac60c6cbaacc98d9882d0ecf4f8d68
+        prob_vida = random.randint(240,1000)
+
 
         all_sprites.draw(screen)
         text_surface = score_font.render("{:08d}".format(score), True, BLACK)
@@ -314,16 +310,16 @@ def tela_play():
         text_rect.midtop = (WIDTH / 2,  10)
         screen.blit(text_surface, text_rect)         
         pg.display.flip()
-<<<<<<< HEAD
+
         
         if player.vida == 0:
             running = False
     
     
-=======
+
 
 def tela_final():
->>>>>>> d590631ae2ac60c6cbaacc98d9882d0ecf4f8d68
+
     running = True
     while running:
         clock.tick(FPS)
@@ -332,7 +328,6 @@ def tela_final():
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
-<<<<<<< HEAD
                     running = False
                     
         # Tela final
@@ -340,7 +335,7 @@ def tela_final():
         # Texto
         # etc
          
-=======
+
                     running = False 
                     return True
                 if event.key == pg.QUIT:
@@ -356,7 +351,7 @@ try:
 
         running = tela_final()
         
->>>>>>> d590631ae2ac60c6cbaacc98d9882d0ecf4f8d68
+
 finally:
     pg.quit()
 
