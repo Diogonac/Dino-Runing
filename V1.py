a7987a0145cbfcc250ab520d949d36fa624e3373
Diogo Cintra <diogonac@al.insper.edu.br>
2019-06-03 15:43:11 -0300

    alteracao

--- V1.py
+++ V1.py
@@ -6,8 +6,6 @@ Email: diogonac@al.insper.edu.br
 Email: luisesfp@al.insper.edu.br
 Email: laurabp@al.insper.edu.br
 """
-
-# Importando as bibliotecas necessárias.
 import pygame as pg
 from os import path
 import random
@@ -15,13 +13,11 @@ from os import path
 fnt_dir = path.join(path.dirname(__file__), 'font')
 img_dir = path.join(path.dirname(__file__), 'img')
 snd_dir = path.join(path.dirname(__file__), 'snd')
-# Inicialização do Pygame.
 pg.init()
 pg.mixer.init()
-
-WIDTH = 600 # Largura da tela
-HEIGHT = 350 # Altura da tela
-FPS = 60 # Frames por segundo
+WIDTH = 600 
+HEIGHT = 350 
+FPS = 60 
 GRAVITY = 0.6
 velocidade = 3
 BLACK = (0,0,0)
@@ -48,49 +44,28 @@ class Player(pg.sprite.Sprite):
         self.image = self.imgs[self.img_index]
         self.rect.y += self.speedy
         self.speedy += GRAVITY
-
         if self.rect.y > HEIGHT:
             self.speedy = 0
             self.rect.y = HEIGHT
         if self.rect.y < 0:
             self.speedy = 0
-            self.rect.y = 0
-            
+            self.rect.y = 0   
         if self.rect.bottom > HEIGHT - 50:
             self.rect.bottom = HEIGHT - 55
             self.pulando = False
-            
-#        if self.rect.y > HEIGHT:
-#            self.rect.y = HEIGHT
-#        if self.rect.y < 0:
-#            self.rect.y = 0
-            
-
         
-            
-
 class Mob(pg.sprite.Sprite):
     #construtor da classe
     def __init__(self, px, py):
-        #construtor classe pai
         pg.sprite.Sprite.__init__(self)
-        #carregando imagem de fundo
         mob_img = pg.image.load(path.join(img_dir, "cacto.png")).convert()
         self.image = mob_img
-        #diminuindo tamanho da imagem
         self.image = pg.transform.scale(mob_img, (40, 32))
-        #detalhes posicao
         self.rect=self.image.get_rect()
-        #sorteia lugar inicial em x
         self.rect.x=px
-        #sorteia lugar y
         self.rect.y=py
- 
-        #sorteia velocidade inicial
-
         self.speedx= -velocidade
         self.speedy= 0
-
         self.image.set_colorkey(WHITE)
 
     def update(self):
@@ -100,25 +75,15 @@ class Mob(pg.sprite.Sprite):
 class Vida(pg.sprite.Sprite):
     #construtor da classe
     def __init__(self, px, py):
-        #construtor classe pai
         pg.sprite.Sprite.__init__(self)
-        #carregando imagem de fundo
         vida_img = pg.image.load(path.join(img_dir, "coracao.png")).convert()
         self.image = vida_img
-        #diminuindo tamanho da imagem
         self.image = pg.transform.scale(vida_img, (40, 32))
-        #detalhes posicao
         self.rect=self.image.get_rect()
-        #sorteia lugar inicial em x
         self.rect.x=px
-        #sorteia lugar y
         self.rect.y=py
- 
-        #sorteia velocidade inicial
-
         self.speedx= -velocidade
         self.speedy= 0
-
         self.image.set_colorkey(BLACK)
 
     def update(self):
@@ -127,23 +92,14 @@ class Vida(pg.sprite.Sprite):
 class Aguia(pg.sprite.Sprite):
     #construtor da classe
     def __init__(self, px, py):
-        #construtor classe pai
         pg.sprite.Sprite.__init__(self)
-        #carregando imagem de fundo
         aguia_img = pg.image.load(path.join(img_dir, "aguia.png")).convert()
         self.image = aguia_img
         self.image.set_colorkey(BLACK)
-        #diminuindo tamanho da imagem
         self.image = pg.transform.scale(aguia_img, (40, 30))
-        #detalhes posicao
         self.rect=self.image.get_rect()
-        #sorteia lugar inicial em x
         self.rect.x=px
-        #sorteia lugar y
         self.rect.y=py
- 
-        #sorteia velocidade inicial
-
         self.speedx= -velocidade
         self.speedy= 0
         
@@ -153,16 +109,14 @@ class Aguia(pg.sprite.Sprite):
         
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 img_dir = path.join(path.dirname(__file__), 'img')
-
-
 pg.display.set_caption("Dino Run")
 clock = pg.time.Clock()
 
-# carregue a imagem do fundo e coloque no background
 background = pg.image.load(path.join(img_dir, 'starfield.png')).convert()
 TI = pg.image.load(path.join(img_dir, 'telainicial.png')).convert()           
 TF = pg.image.load(path.join(img_dir, 'telafinal.png')).convert()  
 score_font = pg.font.Font(path.join(fnt_dir, "PressStart2P.ttf"), 28)
+
 background_rect1 = background.get_rect()
 background_rect2 = background.get_rect()
 background_rect1.x = WIDTH
@@ -177,13 +131,6 @@ all_sprites.add(player)
 all_mobs = pg.sprite.Group()
 all_aguias=pg.sprite.Group()
 all_vida=pg.sprite.Group()
-#for i in range(1):
-#    mob = Mob(WIDTH, 260)
-#    all_sprites.add(mob)
-#    all_mobs.add(mob)
-#
-    
-    
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -192,15 +139,8 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-
-
-# Tamanho da tela.
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-
-# Nome do jogo
 pg.display.set_caption("Dino Run")
-
-# Variável para o ajuste de velocidade
 clock = pg.time.Clock()
 
 def tela_inicial():
@@ -222,7 +162,6 @@ def tela_inicial():
 
 def tela_play():
     player.vida = 3
-    prob_vida = random.randint(60,1000) 
     cont_Mob = 0
     intervalo_Mob = random.randint(FPS//2, 3*FPS)
     cont_aguia=0
@@ -236,8 +175,7 @@ def tela_play():
     
     while running:
         clock.tick(FPS)
-
-            
+          
         if cont_Mob == intervalo_Mob:
             mob = Mob(WIDTH, 260)
             all_sprites.add(mob)
@@ -253,16 +191,17 @@ def tela_play():
             cont_aguia = 0
             intervalo_aguia = random.randint(500, 1000)            
             
-
-            #all_mobs.speedx = -800
-            
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
                 pg.quit()
+                
             if event.type == pg.KEYDOWN:
+                
                 if event.key == pg.K_SPACE:
+                    
                     if not player.pulando:
+                        
                         player.pulando = True
                         player.speedy = -11        
                     
@@ -275,13 +214,16 @@ def tela_play():
         
         hits = pg.sprite.groupcollide(all_players, all_mobs,False, True)
         if not hits:
+            
             score += 1
         if hits:
+            
            player.vida-=1
         hits = pg.sprite.groupcollide(all_players, all_aguias,False, True)
         if hits:
+            
            player.vida-=1
-            #running = False
+
         cont_Mob += 1
         cont_aguia+=1
         cont_life+=1
@@ -290,6 +232,7 @@ def tela_play():
         background_rect2.x -= velocidade
         screen.blit(background, background_rect1)
         screen.blit(background, background_rect2)
+        
         if background_rect1.x<0:
             background_rect1.x = WIDTH
             background_rect2.x = 0
@@ -299,28 +242,11 @@ def tela_play():
             all_sprites.add(vida)
             all_vida.add(vida)
             cont_life = 0
-
-            prob_life = random.randint(240,1000)
+            prob_vida = random.randint(60,1000)
             
-             # Desenha as vidas
-#        text_surface = score_font.render(chr(9829) * player.vida, True, RED)
-#        text_rect = text_surface.get_rect()
-#        text_rect.bottomleft = (10, HEIGHT - 10)
-#        screen.blit(text_surface, text_rect)
-        #print(player.vida)
-            
-        prob_vida = random.randint(240,1000)
-
-
         if (cont % 8) == 0:
             player.img_index = (player.img_index + 1) % 2 
         cont += 1
-
-
-     
-            
-             # Desenha as vidas
-
         all_sprites.draw(screen)
 
         text_surface = score_font.render("{:d}".format(player.vida), True, RED)
@@ -333,89 +259,71 @@ def tela_play():
         text_rect.midtop = (WIDTH / 2,  10)
         screen.blit(text_surface, text_rect)         
         pg.display.flip()
-        print(score)
         
         if score>= 500: 
-            #velocidade = -200   
             aguia.speedx = -5
             mob.speedx = -4
             all_vida.speedx = -4
-
+            
         if score>= 1000: 
-            #velocidade = -200   
             aguia.speedx = -6
             mob.speedx = -5
             all_vida.speedx=-5
- 
+            
         if score>= 1500: 
-            #velocidade = -200   
             aguia.speedx = -7
             mob.speedx = -6
             all_vida.speedx=-6
-
         if score>= 2000: 
-            #velocidade = -200   
             aguia.speedx = -8
             mob.speedx = -7
             all_vida.speedx=-7
 
         if score>= 2500: 
-            #velocidade = -200   
             aguia.speedx = -9
             mob.speedx = -8
             all_vida.speedx= -8
 
         if score>= 3000: 
-            #velocidade = -200   
             aguia.speedx = -10
             mob.speedx = -9
             all_vida.speedx=-9
 
         if score>= 3500: 
-            #velocidade = -200   
             aguia.speedx = -11
             mob.speedx = -10
             all_vida.speedx=-10
 
         if score>= 4000: 
-            #velocidade = -200   
             aguia.speedx = -12
             mob.speedx = -11
             all_vida.speedx= -11
     
-            
         if player.vida == 0:
             running = False
-    
-    
-
 
 def tela_final():
-
     running = True
     while running:
+        
         clock.tick(FPS)
         screen.blit(TF, TF.get_rect())
         pg.display.flip()
         for event in pg.event.get():
+            
             if event.type == pg.KEYDOWN:
+                
                 if event.key == pg.K_SPACE:
 
                     running = False
-                    
-        # Tela final
-        # Background
-        # Texto
-        # etc
-         
+
 
                     running = False 
 
                     return True
                 if event.key == pg.QUIT:
                     return False
-
-
+                
 try: 
     running = True
     while running:
@@ -424,10 +332,5 @@ try:
         tela_play()
         
         running = tela_final()
-
-        
-
-
 finally:
     pg.quit()
-
